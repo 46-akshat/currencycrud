e324e67b06446b7efbc3ea16a168e2391afeb1b3
akshat4619 <akshatjn4619@gmail.com>
2025-06-09 20:55:31 +0530

    Deploy Vite app to gh-pages

--- backend/currency-exchange/currency-exchange/src/main/java/com/akshat/currency_exchange/repo/CurrencyRepo.java
+++
@@ -1,7 +0,0 @@
-package com.akshat.currency_exchange.repo;
-
-import com.akshat.currency_exchange.model.CurrencyRate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyRepo extends JpaRepository<CurrencyRate, Long> {
-}

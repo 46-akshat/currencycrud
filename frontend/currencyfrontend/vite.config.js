7be14fdfffd5016bf6f9e3fbab0bc9887d208804
akshat4619 <akshatjn4619@gmail.com>
2025-06-15 21:55:17 +0530

    add

--- frontend/currencyfrontend/vite.config.js
+++ frontend/currencyfrontend/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // KEEP THIS AS '/' for Vercel
+  base: '/', // Keep this as '/' for Vercel
+  build: {
+    outDir: 'dist', // This is Vite's default, but adding explicitly
+  },
   plugins: [react()],
 })
